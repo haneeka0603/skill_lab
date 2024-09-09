e9ebeba04281ed78b0b292e144ba1efe6d6a0531
haneeka0603 <2320030326@klh.edu.in>
2024-09-09 20:51:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        Logger logger = Logger.getInstance();
+
+        logger.log("Application started.");
+        logger.log("Performing some operations...");
+        logger.log("Application ended.");
+    }
+}
